ba66a6ac5762a687550cd516e14b4cf2d8809724
Ultimatenerd1 <ultimatenerdx00@gmail.com>
2020-07-30 14:29:12 -0500

    Merged in favorites

---
+++ QuickMeals/QuickMeals/Controllers/FavoriteController.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Http;
+using QuickMeals.Models;
+using QuickMeals.Data;
+using Newtonsoft.Json;
+//using QuickMeals.Models.SessionHandlers;
+
+namespace QuickMeals.Controllers
+{
+    public class FavoriteController : Controller
+    {
+        //Trying to get Favorites working - LX
+ 
+            [HttpGet]
+            public ViewResult Index()
+            {
+                var session = new SessionClass(HttpContext.Session);
+                var model = new ListViewModel
+                {
+                    Activeones = session.GetRecipes(),
+                    //activeones = session.GetActiveRec(),
+                    Activetwos = session.GetRecipes(),
+                    //activetwos = session.GetActiveRec(),
+                    Recipes = session.GetRecipes()
+                };
+                return View(model);
+            }
+            [HttpPost]
+            public RedirectToActionResult Delete()
+            {
+                var session = new SessionClass(HttpContext.Session);
+                var cookies = new RecipeCookies(Response.Cookies);
+
+                session.RemoveMyRecipes();
+                cookies.RemoveMyRecipeIds();
+
+                TempData["message"] = "Favorite recipes cleared";
+                return RedirectToAction("Index", "Home",
+                    new
+                    {
+                        ActiveRec = session.GetRecipes(),
+                    //ActiveDiv = session.GetActiveDiv()
+                });
+
+            }
+
+
+            /**
+            public IActionResult Index()
+            {
+                return View();
+            }
+            **/
+    }
+}
---
+++ QuickMeals/QuickMeals/Models/SessionClass.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+
+namespace QuickMeals.Models
+{
+    //created SessionClass for Favorites - lx
+    public class SessionClass
+    {
+        private const string RecipeKey = "myrecipes";
+        private const string CountKey = "recipecount";
+        private const string RecKey = "rec";
+
+        private ISession session { get; set; }
+        public SessionClass(ISession session)
+        {
+            this.session = session;
+        }
+        public void SetMyRecipes(List<Recipe> recipes)
+        {
+            session.SetObject(RecipeKey, recipes);
+            session.SetInt32(CountKey, recipes.Count);
+        }
+        public List<Recipe> GetRecipes() =>
+            session.GetObject<List<Recipe>>(RecipeKey) ?? new List<Recipe>();
+        public int? GetMyTeamCount() => session.GetInt32(CountKey) ?? 0;
+        public void SetActiveRec(string activeRec) =>
+            session.SetString(RecKey, activeRec);
+        public string GetActiveRec() => session.GetString(RecKey);
+
+        public void RemoveMyRecipes()
+        {
+            session.Remove(RecKey);
+            session.Remove(CountKey);
+        }
+    }
+}
