3c3aa9005363fc233b66d97bc4018f1d052efde7
Anna <15157@LAPTOP-S74UCRP4>
2020-08-01 22:56:36 -0500

    Added advanced search page, controller, and model. Updated results page to show text when no results are found.

--- QuickMeals/QuickMeals/Controllers/SearchController.cs
+++ QuickMeals/QuickMeals/Controllers/SearchController.cs
@@ -28,5 +28,51 @@ namespace QuickMeals.Controllers
             // Pass your list out to your view
             return View(recipes);
         }
+
+        
+        //passing instance of AdvancedSearch model to Advanced view.
+        public ActionResult Advanced()
+        {
+            AdvancedSearch ads = new AdvancedSearch();
+            return View(ads);
+        }
+
+        //passing instance of AdvancedSearch model to Advanced view.
+        [HttpPost]
+        public ActionResult Advanced(AdvancedSearch model)
+        {
+            var recipes = context.Recipes.ToList();
+            //Filter by protein
+            if (model.Chicken)
+            {
+                recipes = recipes.Where(p => p.Title.ToLower().Contains("chicken") || p.Description.ToLower().Contains("chicken")).ToList();
+            }
+            if(model.Beef)
+            {
+                recipes = recipes.Where(p => p.Title.ToLower().Contains("beef") || p.Description.ToLower().Contains("beef")).ToList();
+            }
+            if(model.Tofu)
+            {
+                recipes = recipes.Where(p => p.Title.ToLower().Contains("tofu") || p.Description.ToLower().Contains("tofu")).ToList();
+            }
+            if(model.Parsley)
+            {
+                recipes = recipes.Where(p => p.Title.ToLower().Contains("parsley") || p.Description.ToLower().Contains("parsley")).ToList(); 
+            }
+            if(model.Rosemary)
+            {
+                recipes = recipes.Where(p => p.Title.ToLower().Contains("rosemary") || p.Description.ToLower().Contains("rosemary")).ToList();
+            }
+            if(model.Thyme)
+            {
+                recipes = recipes.Where(p => p.Title.ToLower().Contains("thyme") || p.Description.ToLower().Contains("thyme")).ToList();
+            }
+            if (model.Stardust)
+            {
+                recipes = recipes.Where(p => p.Title.ToLower().Contains("stardust") || p.Description.ToLower().Contains("stardust")).ToList();
+            }
+            return View("Results", recipes);
+        }
+
     }
 }
